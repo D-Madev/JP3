9e7cec263b069214ee00b0fe70a1f5b5b1f106c6
D-Malex <3matias.sm@gmail.com>
2025-08-12 16:32:55 -0300

    fix(main): cambio en el router para compatibilidad con GH-PAGES

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router, BrowserRouter } from 'react-router-dom'
 import Inicio from "./pages/Inicio"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <Inicio />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 )
