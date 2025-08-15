dc3686b50e805ed3cebc59a65aa16f2869d23ac8
D-Malex <3matias.sm@gmail.com>
2025-08-15 16:13:12 -0300

    feat(app): cambio en el manejo de rutas y pantallas de carga

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import React, { Suspense, lazy } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/layout/layout.jsx'
+import Loading from './pages/Loading.jsx'
+import Error404 from './pages/Error404.jsx'
+
+const Inicio = lazy(() => import('./pages/Inicio.jsx'))
 
 export default function App() {
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path='inicio' element={<Navigate to='/' replace />} />
+          {/* All the other endpoints */}
+          {/* <Route path='/inicio' element={<Navigate to='/' replace />} /> */}
+          {/* <Route path='/inicio' element={<Navigate to='/' replace />} /> */}
+          {/* <Route path='/inicio' element={<Navigate to='/' replace />} /> */}
+          {/* <Route path='/inicio' element={<Navigate to='/' replace />} /> */}
+          {/* Error 404 */}
+          <Route path='*' element={<Error404 />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router, BrowserRouter } from 'react-router-dom'
-import Inicio from "./pages/Inicio"
+import { HashRouter as Router } from 'react-router-dom' // Import 'BrowserRouter' in production
+import App from "./App"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <Inicio />
+      <App />
     </Router>
   </StrictMode>
 )
