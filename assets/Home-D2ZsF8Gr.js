4d7a62b979bdc0ecf31f01d87c005c3e3f87e114
D-Malex <3matias.sm@gmail.com>
2025-08-16 05:11:50 -0300

    Updates

--- assets/Home-D2ZsF8Gr.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BsPX_bZb.js";const e="_title_59cp9_1",s={title:e};function o(){return t.jsx("h1",{className:s.title,children:"This is the Home Page"})}export{o as default};
---
+++ assets/Home-URkj17L5.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Ci0Hcnsl.js";const e="_title_59cp9_1",s={title:e};function o(){return t.jsx("h1",{className:s.title,children:"This is the Home Page"})}export{o as default};
