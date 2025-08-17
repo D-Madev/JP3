b0ef306634a18f7554a9f95ec0b5560201d62d07
D-Malex <3matias.sm@gmail.com>
2025-08-17 04:32:26 -0300

    feat(ProductCard): creo componente de cards con estilos particulares, view provicional de homw

--- JP3/src/pages/Home/Home.jsx
+++ JP3/src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import styles from './Home.module.css'
+import ProductCard from '../../components/ProductCards/ProductCard'
+
+import image from '../../assets/trex.webp'
+import image1 from '../../assets/tetris-game-0.webp'
+import image2 from '../../assets/triceratops.webp'
 
 export default function Home() {
+  const cards = [
+    {img: image, title: 'T-REX puzzle', price: 20},
+    {img: image1, title: 'Tetris game', price: 30},
+    {img: image2, title: 'Triceratops puzzle', price: 15},
+  ]
+
   return(
     <section className={styles.products}>
       <div className={styles.subProducts}>
         <h2 className={styles.title}>Nuestros productos</h2>
+
+        <ul className={styles.grid} >
+          {cards.map((p, i) => (
+            <li key={p.title + i} className={styles.item} role="listitem">
+              <ProductCard
+                img={p.img}
+                title={p.title}
+                price={p.price}
+              />
+            </li>
+          ))}
+        </ul>
 
         <button className={styles.button}>Ver catálogo</button>
       </div>
